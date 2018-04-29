6003ba9135202e6ea4607990a33a3a9548f9e955
Marco Tello <muchahuev@hotmail.com>
2018-04-29 14:15:33 -0500

    adding mongoose and finishing login, register and events API

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,8 +1,260 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const mongoose = require('mongoose')
+const db = 'mongodb://useradmin:Vidanta01@ds263619.mlab.com:63619/angular5authdb'
+
+mongoose.connect(db, err => {
+    if(err) {
+        console.error('Error! ' + err)
+    } else {
+        console.log('Connected to mongodb')
+    }
+})
+
 
 router.get('/', (req, res) => {
     res.send('From API route')
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if(error) {
+            console.error(error)
+        } else {
+            res.status(200).send(registeredUser)
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+    
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error) {
+            console.log(error)
+        } else {
+            if(!user) {
+                res.status(401).send('Invalid email')
+            } else {
+                if(user.password !== userData.password) {
+                    res.status(401).send('Invalid password')
+                } else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
+})
+
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+    
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error) {
+            console.log(error)
+        } else {
+            if(!user) {
+                res.status(401).send('Invalid email')
+            } else {
+                if(user.password !== userData.password) {
+                    res.status(401).send('Invalid password')
+                } else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
+})
+
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+    
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error) {
+            console.log(error)
+        } else {
+            if(!user) {
+                res.status(401).send('Invalid email')
+            } else {
+                if(user.password !== userData.password) {
+                    res.status(401).send('Invalid password')
+                } else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
+})
+
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
+})
+
+router.get('/special', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2018-05-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
 })
 
 module.exports = router
